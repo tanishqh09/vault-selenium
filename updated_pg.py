4faa071813698f48c7dca2c19c3e09bedef17aaf
tanishqh09 <116097724+tanishqh09@users.noreply.github.com>
2024-08-22 18:28:25 +0530

    Update updated_pg.py

--- updated_pg.py
+++ updated_pg.py
@@ -2,9 +2,11 @@ import os
 import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
+import csv
 import psycopg2
 from sqlalchemy import create_engine
-
+ 
+ 
 stock_code = os.environ.get('STOCK_CODE')
 username = os.environ.get('USERNAME')
 password = os.environ.get('PASSWORD')
@@ -13,40 +15,38 @@ host_ip = os.environ.get('HOST_IP')
 url = f'https://screener.in/company/{stock_code}/consolidated/'
 webpage = requests.get(url)
 soup = bs(webpage.text,'html.parser')
-
 data = soup.find('section', id="profit-loss")
 tdata= data.find("table")
-
 table_data = []
-
+ 
 for row in tdata.find_all('tr'):
     row_data = []
     for cell in row.find_all(['th','td']):
         row_data.append(cell.text.strip())
     table_data.append(row_data)
-
-
+ 
+ 
+# with open("table_data.csv", 'w', newline='') as file:
+#     writer = csv.writer(file)
+#     writer.writerows(table_data)
+ 
+ 
 df_table = pd.DataFrame(table_data)
-df_table= df_table.T
-df_table.iloc[0,0] = 'Year'
+df_table.iloc[0,0] = 'Section'
 df_table.columns = df_table.iloc[0]
-df_table = df_table.iloc[1:]
-df_table = df_table.drop(df_table.index[-1])
-for i in df_table.iloc[:,1:].columns:
-    df_table[i] = df_table[i].str.replace(',','').str.replace('%','').apply(eval)
+df_table = df_table[1:]
 
 df_table['Stock'] = f"{stock_code}" 
 df_table = df_table.reset_index()
-
-
-db_host = host_ip # "192.168.29.101" #
-db_name = "reliance"
+ 
+db_host = host_ip
+db_name = "db"
 db_user = username
 db_password = password
 db_port = "5432"
 
 engine = create_engine(f'postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
-
-df_table.to_sql('profit_loss_data', engine, if_exists='replace', index=True)
-
-print("Data loaded to PostgreSQL")
+# Load the DataFrame into the PostgreSQL database
+df_table.to_sql('profit_loss_data', engine, if_exists='replace', index=False)
+ 
+print("Data loaded successfully into PostgreSQL database!")
